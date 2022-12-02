b490cd62dd03a7f6072d483a7f39fa6545a68212
Logan Baker <loganbakerdev@gmail.com>
2022-12-01 20:31:21 -0800

    changes file path logic

--- helper.js
+++ helper.js
@@ -1,27 +1,16 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const DAY = {
   one: '01'
 }
 
-const getFilePath = (day) => {
-  let filePath = process.cwd();
-
-  // Remove the day from the path if it already exists.
-  // The day is added to the path when loading the data, so not removing it will cause a duplication.
-
-  // EX: advent-of-code-2022/01/01/input.txt
-  if (filePath.includes(day)) {
-    filePath = filePath.replace(day, '');
-  }
-
-  return filePath;
-}
-
 export const loadData = (day) => {
-  const filePath = getFilePath(day);
-
-  return fs.readFileSync(`${filePath}/${day}/input.txt`, 'utf-8')
+  return readFileSync(`${__dirname}/${day}/input.txt`, 'utf-8')
     .split('\n');
 }
 
